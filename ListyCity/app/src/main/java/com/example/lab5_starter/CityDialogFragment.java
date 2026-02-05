20fb9255f749159432eb4dec2302042ea13451c8
Pierce Hampton <pierce.hampton9@gmail.com>
2026-02-05 14:11:43 -0700

    added delete functionality

--- ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
@@ -17,6 +17,8 @@ public class CityDialogFragment extends DialogFragment {
     interface CityDialogListener {
         void updateCity(City city, String title, String year);
         void addCity(City city);
+
+        void deleteCity(City city);
     }
     private CityDialogListener listener;
 
@@ -61,7 +63,7 @@ public class CityDialogFragment extends DialogFragment {
             city = null;}
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        return builder
+        builder
                 .setView(view)
                 .setTitle("City Details")
                 .setNegativeButton("Cancel", null)
@@ -73,7 +75,14 @@ public class CityDialogFragment extends DialogFragment {
                     } else {
                         listener.addCity(new City(title, year));
                     }
-                })
-                .create();
+                });
+
+        if (city != null) {
+            builder.setNeutralButton("Delete", (dialog, which) -> {
+                listener.deleteCity(city);
+            });
+        }
+
+        return builder.create();
     }
 }
